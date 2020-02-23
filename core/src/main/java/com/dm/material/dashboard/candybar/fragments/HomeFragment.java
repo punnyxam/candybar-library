0532687bdd5942aae3ff95d3a56090814e8bd660
punnyxam <61381357+punnyxam@users.noreply.github.com>
2020-02-23 23:20:49 +0700

    Update HomeFragment.java

--- core/src/main/java/com/dm/material/dashboard/candybar/fragments/HomeFragment.java
+++ core/src/main/java/com/dm/material/dashboard/candybar/fragments/HomeFragment.java
@@ -134,13 +134,13 @@ public class HomeFragment extends Fragment implements HomeListener{
     private void initHome() {
         List<Home> homes = new ArrayList<>();
 
-        if (getActivity().getResources().getBoolean(R.bool.enable_apply)&&false) {
-            homes.add(new Home(
-                    R.drawable.ic_toolbar_apply_launcher,
-                    String.format(getActivity().getResources().getString(R.string.home_apply_icon_pack),
-                            getActivity().getResources().getString(R.string.app_name)),
-                    "",
-                    Home.Type.APPLY));
+        if (getActivity().getResources().getBoolean(R.bool.enable_apply)) {
+            //homes.add(new Home(
+            //        R.drawable.ic_toolbar_apply_launcher,
+            //        String.format(getActivity().getResources().getString(R.string.home_apply_icon_pack),
+            //                getActivity().getResources().getString(R.string.app_name)),
+            //        "",
+            //        Home.Type.APPLY));
         }
 
         if (getActivity().getResources().getBoolean(R.bool.enable_donation)) {
